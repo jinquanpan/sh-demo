3472ae8ffdc3bd3412e05627ff5499c68ee196bd
panjinquan <1961782297@qq.com>
2024-02-22 11:54:39 +0800

    feat:添加for脚本

---
+++ for.sh
@@ -0,0 +1,16 @@
+
+# WORKDIR=/Users/wild/project/react/react-project
+# WORKDIR=/Users/wild/project/react/react-project 这是目录路径案例
+
+# echo $PROJECT
+
+
+echo '开始---------------'
+
+projectName=('你' '我' '他')
+
+for i in ${projectName[*]}
+do
+echo $i
+done
+echo "失败$i"
